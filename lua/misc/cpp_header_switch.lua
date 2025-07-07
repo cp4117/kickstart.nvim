54658b110cc47c80c98246988a5321bf576170ab
Chris Pearce <Chris.Pearce@entaingroup.com>
2025-07-07 13:00:39 +0100

    Removing commented out section

--- lua/misc/cpp_header_switch.lua
+++ lua/misc/cpp_header_switch.lua
@@ -3,21 +3,6 @@ local function get_file_ext(path)
   return path:match '^.+(%..+)$'
 end
 
---[[
-local header_source_switch_vertical = function()
-  local buffer_ext = get_file_ext(vim.api.nvim_buf_get_name(0))
-
-  if buffer_ext ~= nil and string.len(buffer_ext) > 0 then
-    print('Buffer ext: ' .. buffer_ext)
-    if buffer_ext == '.cpp' then
-      vim.cmd ':vsp %<.h'
-    elseif buffer_ext == '.h' then
-      vim.cmd ':vsp %<.cpp'
-    end
-  end
-end
---]]
-
 local header_source_switch = function()
   local buffer_ext = get_file_ext(vim.api.nvim_buf_get_name(0))
 
